481b549eb9832c90e3fa405636e9e4e6c2d846f5
João L. Dutra <jdutra.eu@gmail.com>
2021-02-06 23:23:18 -0300

    Update script.js

--- project-2/script.js
+++ project-2/script.js
@@ -1,40 +1,40 @@
-const progress = document.getElementById("progress")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const circles = document.querySelector(".circles")
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
-next.addEventListener("click", () => {
+next.addEventListener('click', () => {
     currentActive++
 
-    if(currentActive > circles.length){
+    if(currentActive > circles.length) {
         currentActive = circles.length
     }
 
     update()
-} )
+})
 
-prev.addEventListener("click", () => {
+prev.addEventListener('click', () => {
     currentActive--
 
-    if(currentActive < 1){
+    if(currentActive < 1) {
         currentActive = 1
     }
 
     update()
-} )
+})
 
-function update(){
+function update() {
     circles.forEach((circle, idx) => {
-        if(idx < currentActive){
-            circle.classList.add("active")
+        if(idx < currentActive) {
+            circle.classList.add('active')
         } else {
-            circle.classList.remove("active")
+            circle.classList.remove('active')
         }
     })
 
     const actives = document.querySelectorAll('.active')
 
-    progress.style.width(actives.length - 1) / (circles.length - 1) * 100 + "%"
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 }
